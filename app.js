9ab1abdaaa247cd0a208e17f76a592089ecaa94c
Adam Miner <miner.adam1@hotmail.com>
2017-06-15 10:52:08 -0600

    further tweaks

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const games = require('./api/games-routes.js');
 const app = express();
+const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use('/api/v1/games', games);
+
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.json({
@@ -13,8 +15,6 @@ app.use(function(err, req, res, next) {
     error: req.app.get('env') === 'development' ? err : {}
   });
 });
-
-const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`Listening on Port ${port}`);
